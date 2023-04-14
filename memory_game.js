f803f49061681b73a399ad91997297f91a0f5cab
yonatan a <yonatanriel@gmail.com>
2023-04-14 09:55:10 +0300

    update css design + stopwatch function

--- memory_game.js
+++ memory_game.js
@@ -1,6 +1,7 @@
-let arr1 = ["a","b","c","d","a","b","c","d"];
-let arr2 = ["a","b","c","d","a","b","c","d"];
-console.log(arr1)
+let arr1 = ["a","b","c","d","e","f","g","h","i"];
+let userChoose = 13 //with how many cards the user want to play (minimum 4 - max 18?)//need to let him choose levels or just even numbers
+let arr2 = arr1.slice(0,userChoose / 2).concat(arr1.slice(0,userChoose / 2))
+console.log(arr1,arr2)
 let startGame = false //if the user press start - startGame = true
 //function that suffle the second array
 function shuffle(arr){
@@ -33,7 +34,8 @@ function showCards(starsArr,shufArray,index1,index2){
     return arr
 }
 
-let shufarray = shuffle(arr1);
+let shufarray = shuffle(arr2);
+console.log(shufarray)
 let starSuffleArr = arrToStar(shufarray)
 let userOutput1 = 2; //first index (of card) from the user
 showCards(starSuffleArr, shufarray, userOutput1); //show the user the first card
@@ -45,26 +47,29 @@ if(shufarray[userOutput1] == shufarray[userOutput2]){//if the cards matches
 }
 else{
     showCards(starSuffleArr, shufarray, userOutput1, userOutput2)//show the cars for 5 seconds
-   // setTimeout(starSuffleArr, 5000)
+   // setTimeout(starSuffleArr, 5000)hide() //show() delay() fadeout() //need to study "jQuery"
 }
 //func - to count how may moves the user did
 function moves(moves){
     moves++
 }
-let seconds = 0
-let minutes = 0
+let seconds = 0;
+let minutes = 0;
 //stopwatch
 function stopWatch(){
     if(minutes == 99 && seconds > 58){
-        return "to much time past, you lost"
+        return "too much time past, you lost"
     }
     else if(seconds == 60){
         minutes++
         seconds = 0
     }
     seconds++  
+    console.log(`${minutes}:${seconds}`) 
 }
 setInterval(stopWatch,1000)
+//setInterval(bb,1000)
+//setTimeOut()?
 //scoring method
 function score(){
     if(timer < 200 && moves < 10){
