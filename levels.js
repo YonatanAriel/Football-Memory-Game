239db823a5e46f884b843cb69b3c8edff39f03f3
Yonatan Ariel <yonatanriel@gmail.com>
2023-08-08 09:49:35 +0300

    some responsive work

--- levels.js
+++ levels.js
@@ -1,23 +1,28 @@
 let difficulty;
 let cardsAmount;
+let container = document.getElementById("container");
 let easy = document.getElementById("easy"); 
 let nightmare = document.getElementById("nightmare");
-// let hard = document.getElementById("hard");
+let instructionElement = document.getElementById("h2");
 let levels = [easy,nightmare];
 levels.forEach(level =>  level.onclick = levelClick);
-let userChoiceH1 = document.getElementById("h2");
+
 function levelClick(event){
     choice = event.target;
-    userChoiceH1.innerText = "Cards amount:";
-    // userChoiceH1.style.fontSize = "100px";
+    container.style.opacity = "0";
     difficulty = choice.innerText;
     localStorage.setItem("difficulty",difficulty);
-    console.log(cardsAmount, difficulty)
-    easy.innerText = "24";
-    nightmare.textContent = "48";
-    levels.forEach(p => { p.style.fontSize = "100px" 
-    p.onclick = amount} );
+
+    setTimeout(() => {
+        container.style.opacity = "1";
+        instructionElement.innerText = "Cards amount"; 
+        easy.innerText = "24";
+        nightmare.textContent = "48";
+        levels.forEach(p => { p.style.fontSize = "100px" 
+        p.onclick = amount} );    
+    }, 500);
 } 
+
 function amount(event){
     cardsAmount = event.target.innerText;
     localStorage.setItem("cardAmount",cardsAmount);
